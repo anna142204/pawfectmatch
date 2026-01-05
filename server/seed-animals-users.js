e3540e1dbfe9a6a953de404b3b0a0d28a8aa1ef1
Dorytos13 <doriane.rosset@heig-vd.ch>
2026-01-05 15:06:58 +0100

    add animals to seeder

--- server/seed-animals-users.js
+++ server/seed-animals-users.js
@@ -142,6 +142,90 @@ const seed = async () => {
                 desc: 'Un nez incroyable ! Attention à ne pas laisser traîner de nourriture.',
                 env: ['voiture', 'enfant'], dress: ['têtu', 'habitué à la laisse'], pers: ['curieux', 'indépendant', 'sociable']
             },
+            {
+                name: 'Daxon', species: 'chien', race: 'Autre / Croisé', age: '0-1', sex: 'male', size: 'moyen', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619478/Daxon_Puppy_y0mhst.jpg'],
+                desc: 'Un petit aventurier très attentif qui adore porter son harnais pour partir en promenade.',
+                env: ['enfant', 'autre animaux', 'voiture'], dress: ['facile à dresser'], pers: ['curieux', 'sociable', 'énergique']
+            },
+            {
+                name: 'Cute', species: 'chien', race: 'Autre / Croisé', age: '0-1', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619481/Cute_Puppy_-_panoramio_kdpaj7.jpg'],
+                desc: 'Une petite boule de poils noire très calme qui aime se blottir dans des endroits douillets.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['calme', 'affectueux', 'timide']
+            },
+            {
+                name: 'Blacky', species: 'chien', race: 'Autre / Croisé', age: '0-1', sex: 'male', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619481/Black_puppy_in_Bhutan_jg33fp.jpg'],
+                desc: 'Un chiot courageux qui n’a pas peur de se salir les pattes lors des explorations en extérieur.',
+                env: ['autre animaux', 'chien'], dress: ['têtu'], pers: ['explorateur', 'énergique', 'curieux']
+            },
+            {
+                name: 'Zar', species: 'chien', race: 'Borzoi', age: '0-1', sex: 'male', size: 'grand', weight: '10-20',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619519/Borzoi_puppy_on_sofa_0293_zbrrxw.jpg'],
+                desc: 'Très élégant avec son long museau, il est déjà très calme et posé pour son jeune âge.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['calme', 'indépendant', 'câlin']
+            },
+            {
+                name: 'Shorty', species: 'chien', race: 'Welsh Corgi', age: '0-1', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619480/Corgi_Puppy_wgmo4p.jpg'],
+                desc: 'Petite par la taille mais grande par le caractère, elle saura vous faire rire avec ses expressions.',
+                env: ['appartement', 'enfant', 'voiture'], dress: ['facile à dresser'], pers: ['joueur', 'sociable', 'curieux']
+            },
+            {
+                name: 'Barbarella', species: 'chien', race: 'Chien Chinois à Crête', age: '0-1', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619482/Barbarella_the_Chinese_crested_puppy__72353_vydrjd.jpg'],
+                desc: 'Une petite chienne au look unique et exotique, extrêmement attachée à ses maîtres.',
+                env: ['appartement', 'autre animaux'], dress: ['facile à dresser'], pers: ['affectueux', 'câlin', 'sociable']
+            },
+            {
+                name: 'Cloudy', species: 'chien', race: 'Samoyède', age: '0-1', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619479/Cloudy_puppy_u4vlix.jpg'],
+                desc: 'Aussi blanc et doux qu’un nuage, il est toujours en quête d’une nouvelle bêtise à faire.',
+                env: ['appartement', 'chien'], dress: ['têtu'], pers: ['énergique', 'joueur', 'curieux']
+            },
+            {
+                name: 'Reggie', species: 'chien', race: 'Autre / Croisé', age: '3-7', sex: 'female', size: 'petit', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619520/Sitting_dogs_in_Philippines__Region_3__2023-07-26__E911a_13_c7ytn8.jpg'],
+                desc: 'Une petite lady très bien éduquée qui adore porter ses vêtements d’hiver pour rester au chaud.',
+                env: ['appartement', 'enfant', 'voiture'], dress: ['éduqué'], pers: ['sociable', 'calme', 'affectueux']
+            },
+            {
+                name: 'Polux', species: 'chien', race: 'Bouvier d’Entlebuch', age: '0-1', sex: 'male', size: 'moyen', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619483/Entlebucher_Puppy_on_lawn_f13jfr.jpg'],
+                desc: 'Un regard irrésistible et une loyauté sans faille. Il apprend très vite les nouveaux ordres.',
+                env: ['enfant', 'chien', 'autre animaux'], dress: ['facile à dresser'], pers: ['protecteur', 'affectueux', 'énergique']
+            },
+            {
+                name: 'Bayka', species: 'chien', race: 'Autre / Croisé', age: '0-1', sex: 'female', size: 'moyen', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619479/Bayka_2_yvdzln.jpg'],
+                desc: 'Une magnifique chienne aux yeux clairs, très vive et prête à découvrir le monde.',
+                env: ['voiture', 'chien', 'enfant'], dress: ['habitué à la laisse'], pers: ['explorateur', 'curieux', 'énergique']
+            },
+            {
+                name: 'Sherpa', species: 'chien', race: "Berger de l'Himalaya", age: '7+', sex: 'male', size: 'grand', weight: '30+',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619521/Gaddi_dogs_of_Himachal_s5pftv.jpg'],
+                desc: 'Un vieux sage à la fourrure épaisse, habitué au grand air et aux climats frais.',
+                env: ['autre animaux', 'chien'], dress: ['têtu'], pers: ['indépendant', 'calme', 'territorial']
+            },
+            {
+                name: 'Woof', species: 'chien', race: 'Alaskan Malamute', age: '3-7', sex: 'female', size: 'grand', weight: '30+',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619522/Alaskan_Malamute_R_Bartz_mwriaz.jpg'],
+                desc: 'Une force de la nature qui ne demande qu’à courir dans de grands espaces. Très fidèle.',
+                env: ['enfant', 'chien', 'voiture'], dress: ['habitué à la laisse'], pers: ['énergique', 'explorateur', 'sociable']
+            },
+            {
+                name: 'Tyson', species: 'chien', race: 'Boxer', age: '3-7', sex: 'male', size: 'moyen', weight: '20-30',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767619524/images_2_ulwsw6.jpg'],
+                desc: 'Un chien athlétique et fier qui adore passer ses après-midi à surveiller le jardin depuis l’herbe.',
+                env: ['enfant', 'voiture'], dress: ['éduqué'], pers: ['calme', 'protecteur', 'affectueux']
+            },
+            {
+                name: 'Praline', species: 'chien', race: 'Caniche', age: '3-7', sex: 'female', size: 'petit', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620620/Dogs_of_Taiwan_-_20180429_-_02_ghptvx.jpg', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620620/Dogs_of_Taiwan_-_20180429_-_03_yobb9r.jpg', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620621/Dogs_of_Taiwan_-_20180429_-_04_qfec9h.jpg'],
+                desc: 'Très élégante avec son petit nœud bleu, elle adore être le centre de l’attention lors des grands événements.',
+                env: ['appartement', 'enfant', 'voiture'], dress: ['éduqué'], pers: ['sociable', 'affectueux', 'joueur']
+            },
 
             // CHATS
             {
@@ -180,6 +264,54 @@ const seed = async () => {
                 desc: 'Très vocale, elle vous racontera toute sa journée quand vous rentrez.',
                 env: ['appartement'], dress: ['éduqué'], pers: ['bavard', 'affectueux', 'curieux']
             },
+            {
+                name: 'Fuji', species: 'chat', race: 'Siamois', age: '0-1', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617600/Capture_d_%C3%A9cran_2026-01-05_131213_tftnlk.png', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617601/Capture_d_%C3%A9cran_2026-01-05_131205_oc2juh.png'],
+                desc: 'Très vocale, elle vous racontera toute sa journée quand vous rentrez.',
+                env: ['appartement', 'autre animaux', 'enfant'], dress: ['facile à dresser'], pers: ['sociable', 'affectueux', 'curieux']
+            },
+            {
+                name: 'Simba', species: 'chat', race: 'Maine Coon', age: '3-7', sex: 'male', size: 'grand', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617580/64003442-3412-4284-93B0-F3526B486CAE_1_105_c-r3j5jgsy85l98i9r3dlmakj3jgue9niglgpascfs9e_bivywb.webp'],
+                desc: 'Un véritable géant au cœur tendre qui adore observer le jardin depuis son perchoir.',
+                env: ['enfant', 'chien', 'autre animaux'], dress: ['éduqué'], pers: ['calme', 'affectueux', 'protecteur']
+            },
+            {
+                name: 'Luna', species: 'chat', race: 'Bengal', age: '1-3', sex: 'female', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617580/4089182635_2fac8ab56b_b_kh0mu9.jpg'],
+                desc: 'Très active et athlétique, elle a besoin de beaucoup de stimulation et de jeux interactifs.',
+                env: ['appartement', 'chat'], dress: ['facile à dresser', 'habitué à la laisse'], pers: ['énergique', 'joueur', 'explorateur']
+            },
+            {
+                name: 'Missou', species: 'chat', race: 'British Shorthair', age: '0-1', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617581/Silver_Classic_Tabby_British_Shorthair_Kitten_owgvag.jpg'],
+                desc: 'Toujours à la recherche de chaleur, il passera ses journées blotti contre vous ou sous un plaid.',
+                env: ['appartement', 'enfant'], dress: ['facile à dresser'], pers: ['sociable', 'câlin', 'curieux']
+            },
+            {
+                name: 'Bobine', species: 'chat', race: 'Bleu Russe', age: '7+', sex: 'female', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617579/12971548714_b4cbbaf81d_b_fhshld.jpg'],
+                desc: 'Une demoiselle discrète qui apprécie le calme. Elle est un peu réservée au premier abord.',
+                env: ['appartement'], dress: ['éduqué'], pers: ['calme', 'timide', 'indépendant']
+            },
+            {
+                name: 'Biscuit', species: 'chat', race: 'British Shorthair', age: '1-3', sex: 'male', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617580/British_shorthair_house_cat_Mio_5_months_old_tvhpht.jpg'],
+                desc: 'Avec sa bouille ronde et ses beaux yeux, il fait craquer tout le monde. Très zen.',
+                env: ['appartement', 'enfant', 'chat'], dress: ['éduqué'], pers: ['calme', 'affectueux', 'sociable']
+            },
+            {
+                name: 'Jack', species: 'chat', race: 'Européen (Gouttière)', age: '3-7', sex: 'male', size: 'moyen', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617579/White_Domestic_Shorthair_Cat_dholdj.jpg'],
+                desc: 'Un aventurier qui a son petit caractère. Il sait exactement ce qu’il veut.',
+                env: ['autre animaux', 'voiture'], dress: ['têtu'], pers: ['indépendant', 'explorateur', 'territorial']
+            },
+            {
+                name: 'Piplette', species: 'chat', race: 'Maine Coon', age: '1-3', sex: 'female', size: 'grand', weight: '5-10',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767617579/Chat_cr%C3%A8me_yqkg7h.jpg'],
+                desc: 'Une petite pile électrique qui grimpe partout. Sa curiosité n’a aucune limite.',
+                env: ['appartement', 'chien'], dress: ['facile à dresser'], pers: ['curieux', 'énergique', 'bavard']
+            },
             
             // AUTRES
             {
@@ -205,7 +337,79 @@ const seed = async () => {
                 images: ['https://images.unsplash.com/photo-1552071379-05006b5276e0?auto=format&fit=crop&w=800&q=80'],
                 desc: 'Magnifique plumage. Il a besoin de beaucoup d\'attention et d\'espace.',
                 env: ['autre animaux'], dress: ['têtu'], pers: ['bavard', 'territorial']
-            }
+            },
+            {
+                name: 'Pikachu', species: 'oiseau', race: 'Perruche Calopsitte', age: '1-3', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621291/images_me0ysp.jpg'],
+                desc: 'Reconnaissable à ses joues oranges et sa crête expressive, il adore siffler pour attirer l’attention.',
+                env: ['appartement', 'enfant'], dress: ['facile à dresser'], pers: ['sociable', 'curieux', 'bavard']
+            },
+            {
+                name: 'Etoile et Soleil', species: 'oiseau', race: 'Inséparable', age: '1-3', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621447/images_1_ttnria.jpg'],
+                desc: 'Un duo inséparable aux couleurs éclatantes qui passera ses journées à bavarder et à prendre soin l’un de l’autre.',
+                env: ['appartement'], dress: ['têtu'], pers: ['affectueux', 'énergique', 'territorial']
+            },
+            {
+                name: 'Colette', species: 'oiseau', race: 'Poule / Coq', age: '1-3', sex: 'female', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621451/AdobeStock_1840717259_lhzb7n.jpg'],
+                desc: 'Une poule rousse très sociable qui adore fouiller le sol à la recherche de petits insectes.',
+                env: ['autre animaux', 'enfant'], dress: ['éduqué'], pers: ['calme', 'explorateur', 'sociable']
+            },
+            {
+                name: 'Azur', species: 'oiseau', race: 'Perruche Ondulée', age: '1-3', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621449/blue-domestic-parrot-in-cage_ufzf57.jpg'],
+                desc: 'Un petit oiseau aux couleurs éclatantes qui adore observer son environnement depuis son perchoir.',
+                env: ['appartement', 'autre animaux'], dress: ['facile à dresser'], pers: ['curieux', 'énergique', 'joueur']
+            },
+            {
+                name: 'Bob', species: 'oiseau', race: 'Canari', age: '1-3', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621698/AdobeStock_171341824_g5zbcy.jpg', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621712/AdobeStock_171342063_iwopld.jpg'],
+                desc: 'Un petit chanteur joyeux qui adore grignoter ses graines et faire sa toilette le matin.',
+                env: ['appartement'], dress: ['éduqué'], pers: ['calme', 'sociable', 'curieux']
+            },
+            {
+                name: 'Dalmatien', species: 'lapin', race: 'Lapin Rex', age: '1-3', sex: 'male', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620853/Dalmatian_Pattern_Mini_Rex_ualm1l.jpg'],
+                desc: 'Un lapin magnifique avec un pelage court et extrêmement doux au toucher, rappelant le velours.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['calme', 'curieux', 'sociable']
+            },
+            {
+                name: 'Ciurbi', species: 'lapin', race: 'Lapin Tête de Lion', age: '1-3', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620854/Ciurbi_dolodc.jpg'],
+                desc: 'Un petit compagnon curieux avec une légère crinière de poils plus longs sur le sommet de la tête.',
+                env: ['appartement', 'enfant', 'autre animaux'], dress: ['éduqué'], pers: ['curieux', 'sociable', 'calme']
+            },
+            {
+                name: 'Ebène', species: 'lapin', race: 'Lapin Bélier', age: '0-1', sex: 'female', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620855/Holland_Lop_Bunny_Standing_On_Her_Back_Feet_jiltth.jpg'],
+                desc: 'Une petite boule de poils toute noire avec des oreilles tombantes adorables. Elle aime se tenir debout pour quémander des friandises.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['curieux', 'câlin', 'joueur']
+            },
+            {
+                name: 'Yogurt', species: 'lapin', race: 'Lapin Angora', age: '1-3', sex: 'female', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620856/Yoghurtkanin25_aq3xdo.jpg'],
+                desc: 'Une véritable boule de coton sur pattes qui demande un brossage régulier pour garder son poil soyeux.',
+                env: ['appartement', 'autre animaux'], dress: ['éduqué'], pers: ['calme', 'timide', 'affectueux']
+            },
+            {
+                name: 'Caramel', species: 'lapin', race: 'Lapin de Garenne', age: '1-3', sex: 'male', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767620857/Pet_rabbit_in_a_cage_hnae8u.jpg'],
+                desc: 'Un lapin vif et alerte qui adore grignoter son foin tout en observant ce qui se passe autour de sa cage.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['curieux', 'énergique', 'timide']
+            },
+            {
+                name: 'Panpan', species: 'lapin', race: 'Lapin Nain', age: '1-3', sex: 'male', size: 'petit', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621109/pexels-photo-3820509_injhla.jpg'],
+                desc: 'Un petit lapin très vif avec un marquage original en forme de masque. Il est très curieux de son environnement.',
+                env: ['appartement', 'enfant'], dress: ['éduqué'], pers: ['énergique', 'curieux', 'joueur']
+            },
+            {
+                name: 'Nougat', species: 'lapin', race: 'Lapin Bélier', age: '1-3', sex: 'female', size: 'moyen', weight: '0-5',
+                images: ['https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621180/640px-Pet_Bunny_4_2013-08-22_hcpsjn.jpg', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621178/640px-Pet_Bunny_6_2013-07-27_w12j1v.jpg', 'https://res.cloudinary.com/dzs3mwpgx/image/upload/v1767621178/Bunny_4_2013-07-29_lwkmzl.jpg'],
+                desc: 'Adorable avec ses taches rousses et ses longues oreilles tombantes, elle est d’un naturel très calme.',
+                env: ['appartement', 'autre animaux'], dress: ['éduqué'], pers: ['calme', 'affectueux', 'sociable']
+            },
         ];
 
         console.log('Création des animaux...');
